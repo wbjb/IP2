9e5a755130b5372ba8020399bca5c46719078c74
cmdr33 <driveguy44@gmail.com>
2018-11-30 03:05:15 -0500

    Created a starter-working program that can identify an equilateral triangle.

---
+++ script.js
@@ -0,0 +1,13 @@
+a = ""
+b = ""
+c = ""
+
+for (count = 0; count < 1; count+=1){
+	a = parseInt(prompt("Enter the first measurement"))
+  b = parseInt(prompt("Enter the second measurement"))
+  c = parseInt(prompt("Enter the third measurement"))
+  
+}
+if (a == b && b == c){
+	print ("Equiangular")
+}
