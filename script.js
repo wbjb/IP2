79bc88ebe9293790762fad29ed983fae99179fa6
cmdr33 <driveguy44@gmail.com>
2018-11-30 03:45:14 -0500

    Completed the triangle tracker and added some functions and an array in the script.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ a = ""
 b = ""
 c = ""
 
+types = ["Isosceles","Scalene","Equilateral","Not a triangle"]
+
+function iso(isosceles){
+return(types[0])
+}
+
 for (count = 0; count < 1; count+=1){
 	a = parseInt(prompt("Enter the first measurement"))
   b = parseInt(prompt("Enter the second measurement"))
@@ -9,5 +15,26 @@ for (count = 0; count < 1; count+=1){
   
 }
 if (a == b && b == c){
-	print ("Equiangular")
+	print (types[2])}
+ else if (a + c == b || b + c == a || a + b == c){
+	print (types[3])
+
 }
+
+else if (a == b && b !=c){
+	iso()
+
+} 
+else if (a != b && b ==c){
+	iso()
+
+}
+else if (a == c && b !=c){
+	iso()
+
+}
+else if (a != c && b !=c){
+	print (types[1])
+
+}
+
