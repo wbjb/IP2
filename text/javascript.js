469d6dd41e6a6f097f8138e5ded64fe0c438e4bd
cmdr33 <driveguy44@gmail.com>
2018-12-03 01:27:55 -0500

    Added more functions.

--- text/javascript.js
+++ text/javascript.js
@@ -7,6 +7,12 @@ types = ["Isosceles","Scalene","Equilateral","Not a triangle"]
 function iso(isosceles){
 return(alert(types[0]))
 }
+function equal(equiangular){
+	return (alert(types[2]))
+}
+function Nan(notatriangle){
+	return (alert(types[3]))
+}
 
 for (count = 0; count < 1; count+=1){
 	a = parseInt(prompt("Enter the first measurement"))
@@ -15,9 +21,9 @@ for (count = 0; count < 1; count+=1){
   
 }
 if (a == b && b == c){
-	print (types[2])}
+	equal()}
  else if (a + c == b || b + c == a || a + b == c || a + c < b || b + c < a || a + b < c){
-	alert (types[3])
+	Nan()
 
 }
 
